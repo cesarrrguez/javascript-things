0a844fa2f0d43d17af1d1a9a48f835516d80e13d
César Rodríguez <cesar.rrguez@gmail.com>
2020-11-18 10:04:46 +0100

    Add array filter example

---
+++ src/functionalProgramming/arrayFilter.js
@@ -0,0 +1,14 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const users = [
+  { firstName: 'James', lastName: 'Smith', age: 27 },
+  { firstName: 'John', lastName: 'Smith', age: 35 },
+  { firstName: 'Olivia', lastName: 'Johnson', age: 43 },
+  { firstName: 'Michael', lastName: 'Jones', age: 19 },
+];
+
+const filteredNumbers = numbers.filter((x) => x < 5);
+console.log(filteredNumbers);
+
+const filteredUsers = users.filter((x) => x.age > 30);
+console.log(filteredUsers);
