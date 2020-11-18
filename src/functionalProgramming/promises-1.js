d8adc8141fa36f4e2779f36e15c1c9fc5fdf7699
César Rodríguez <cesar.rrguez@gmail.com>
2020-11-18 17:35:56 +0100

    Add promises examples

---
+++ src/functionalProgramming/promises-1.js
@@ -0,0 +1,10 @@
+/* eslint-disable prefer-promise-reject-errors */
+
+const p1 = Promise.resolve(1);
+console.log(p1);
+
+p1.then((x) => x + 5)
+  .then((x) => Promise.resolve(x + 5))
+  .then(() => Promise.reject('There was an error :('))
+  .then(() => console.log('this is not going to be called'))
+  .catch((e) => console.log(e));
---
+++ src/functionalProgramming/promises-2.js
@@ -0,0 +1,12 @@
+/* eslint-disable prefer-promise-reject-errors */
+
+const delayed = (x) => new Promise((resolve) => setTimeout(() => resolve(x), 900));
+
+delayed(7)
+  .then((x) => {
+    console.log(x);
+    return delayed(7 + 7);
+  })
+  .then((x) => console.log(x))
+  .then(() => Promise.reject('There was an error :('))
+  .catch((e) => console.log(e));
