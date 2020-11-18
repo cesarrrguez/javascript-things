5da98a5e0c9abe86be52bcf49fc5c848a9224abd
César Rodríguez <cesar.rrguez@gmail.com>
2020-11-18 13:01:47 +0100

    Add trampolines example

---
+++ src/functionalProgramming/trampolines.js
@@ -0,0 +1,20 @@
+// Without trampoline
+const sum = (number, acc = 0) => (number === 0 ? acc : sum(number - 1, acc + number));
+
+const sumResult = sum(1000); // > 10000 -> Maximum call stack size exceeded
+console.log(sumResult);
+
+// With trampoline
+const trampoline = (fn) => (...args) => {
+  let result = fn(...args);
+  while (typeof result === 'function') {
+    result = result();
+  }
+  return result;
+};
+
+const tSum = (number, acc = 0) => (number === 0 ? acc : () => tSum(number - 1, acc + number));
+
+const trampolineSum = trampoline(tSum);
+const trampolineSumResult = trampolineSum(10000);
+console.log(trampolineSumResult);
