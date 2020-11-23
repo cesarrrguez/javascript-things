6c7df8ed0eaba64e03c6a7deb363b63fd72ed98b
César Rodríguez <cesar.rrguez@gmail.com>
2020-11-23 21:58:22 +0100

    Rename folders and file names to kebab-case convention

--- src/functional-programming/promises/example-3.js
+++ src/functional-programming/promises/example-3.js
@@ -1,5 +1,5 @@
 require('isomorphic-fetch');
 
 fetch('https://jsonplaceholder.typicode.com/users')
-  .then((x) => x.json())
-  .then((x) => console.log(x));
+  .then((response) => response.json())
+  .then((data) => console.log(data));
