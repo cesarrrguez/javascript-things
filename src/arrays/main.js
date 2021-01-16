9100c99fc3e69b6b775e9e7ba4281730b2cb87f2
César Rodríguez <cesar.rrguez@gmail.com>
2021-01-16 18:15:43 +0100

    Include new arrays operations

--- src/arrays/main.js
+++ src/arrays/main.js
@@ -1,4 +1,5 @@
 const numbers = [1, 3, 3, 3, 1];
+console.log(numbers); // [1, 3, 3, 3, 1]
 
 const numbersMap = numbers.map((x) => x * 2);
 console.log(numbersMap); // [ 2, 6, 6, 6, 2 ]
@@ -15,6 +16,9 @@ console.log(numbersIndexOf); // 0
 const numbersLastIndexOf = numbers.lastIndexOf(1);
 console.log(numbersLastIndexOf); // 4
 
+const numbersInclude = numbers.includes(1);
+console.log(numbersInclude); // true
+
 const numbersSome = numbers.some((x) => x === 1);
 console.log(numbersSome); // true
 
@@ -23,3 +27,47 @@ console.log(numbersEvery); // false
 
 const numbersReduce = numbers.reduce((acc, item) => acc + item, 0);
 console.log(numbersReduce); // 11
+
+const isArray = Array.isArray(numbers);
+console.log(typeof numbers); // object
+console.log(isArray); // true
+
+numbers[0] = 21;
+const firstNumber = numbers[0];
+console.log(firstNumber); // 21
+
+const total = numbers.push(5, 9);
+console.log(numbers); // [ 21, 3, 3, 3, 1, 5, 9 ]
+console.log(total); // 7
+
+const last = numbers.pop();
+console.log(numbers); // [ 21, 3, 3, 3, 1, 5 ]
+console.log(last); // 9
+
+const first = numbers.shift();
+console.log(numbers); // [ 3, 3, 3, 1, 5 ]
+console.log(first); // 21
+
+const len = numbers.unshift(1);
+console.log(numbers); // [ 1, 3, 3, 3, 1, 5 ]
+console.log(len); // 6
+
+const cut = numbers.slice(2, 4);
+console.log(cut); // [ 3, 3 ]
+
+numbers.splice(2, 3, 7, 9);
+console.log(numbers); // [ 1, 3, 7, 9, 5 ]
+
+numbers.forEach((x) => console.log(x));
+// 1
+// 3
+// 7
+// 9
+// 5
+
+numbers.forEach((x, index) => console.log(`${x} - ${index}`));
+// 1 - 0
+// 3 - 1
+// 7 - 2
+// 9 - 3
+// 5 - 4
