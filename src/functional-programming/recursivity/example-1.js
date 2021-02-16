64991835daec5f303949acbe1b53d2c544a38074
César Rodríguez <cesar.rrguez@gmail.com>
2021-02-16 11:18:10 +0100

    Change recursivity example

--- src/functional-programming/recursivity/example-1.js
+++ src/functional-programming/recursivity/example-1.js
@@ -1,7 +1,24 @@
-const countDown = (a) => {
-  if (a < 0) return false;
-  console.log(a);
-  return countDown(a - 1);
+const countDown = (n) => {
+  let counter = n;
+  const result = [];
+
+  while (counter) {
+    result.push(counter--);
+  }
+
+  return result;
 };
 
-countDown(10);
+const countDownRecursive = (n) => {
+  if (n === 0) return [];
+
+  return [n].concat(countDownRecursive(n - 1));
+};
+
+// Iterative
+console.log(countDown(1)); // [1]
+console.log(countDown(5)); // [5, 4, 3, 2, 1]
+
+// Recursive
+console.log(countDownRecursive(1)); // [1]
+console.log(countDownRecursive(5)); // [5, 4, 3, 2, 1]
