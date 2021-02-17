c62abfc8e4fdc05be72974fd8514ae4d2e7392e5
César Rodríguez <cesar.rrguez@gmail.com>
2021-02-17 09:10:15 +0100

    Add email example

---
+++ src/email/main.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'cesar.rrguez@gmail.com',
+    pass: '*************',
+  },
+  tls: { rejectUnauthorized: false },
+});
+
+const mailOptions = {
+  from: 'cesar.rrguez@gmail.com',
+  to: 'cesar.rrguez@gmail.com',
+  subject: 'Test email subject',
+  html: '<p>Test email <strong>body</strong></p>',
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.log('Error sending email');
+    console.log(error);
+  } else {
+    console.log('Email sent successfully');
+    console.log(info);
+  }
+});
+
+transporter.close();
