d9a4ef96fe5b902736a4cc849206fce9e8c4652a
César Rodríguez <cesar.rrguez@gmail.com>
2021-02-22 14:08:14 +0100

    Add console example

---
+++ src/console/main.js
@@ -0,0 +1,18 @@
+// console.log()
+console.log('Hello World');
+console.log([1, 2, 3]);
+console.log({ x: 1, y: 2, z: 3 });
+
+// console.table()
+console.table({ x: 1, y: 2, z: 3 });
+
+// console.error()
+console.error('Error founded');
+
+// console.warn()
+console.warn('Warning!');
+
+// console.group()
+console.group('Group');
+console.log('Inside group');
+console.groupEnd('Group');
