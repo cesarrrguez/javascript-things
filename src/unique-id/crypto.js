8020aea3430068d6b6918829f90318eb1a6303e9
César Rodríguez <cesar.rrguez@gmail.com>
2023-02-10 14:09:07 +0100

    Add unique id examples

---
+++ src/unique-id/crypto.js
@@ -0,0 +1,4 @@
+import { randomUUID } from 'crypto';
+
+const id = randomUUID();
+console.log(id); // e6f8b8f8-f8f8-4f8f-8f8f-8f8f8f8f8f8f
---
+++ src/unique-id/custom.js
@@ -0,0 +1,2 @@
+const id = Math.random().toString(36).slice(2);
+console.log(id); // j0c4wld2eb
