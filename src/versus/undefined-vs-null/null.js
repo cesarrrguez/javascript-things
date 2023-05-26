3ce99861f12e42f69019d9133468d81aef2e0ed6
César Rodríguez <cesar.rrguez@gmail.com>
2023-05-26 20:14:23 +0200

    Add null vs undefinied

---
+++ src/versus/undefined-vs-null/null.js
@@ -0,0 +1,7 @@
+// Null: The variable is defined but has no value
+
+const song = null;
+
+console.log(song); // null
+
+console.log(typeof null); // object
---
+++ src/versus/undefined-vs-null/undefined.js
@@ -0,0 +1,13 @@
+// Undefined: The variable is not defined or not exists
+
+let name;
+
+console.log(name); // undefined
+
+const obj = {
+  name: 'John',
+};
+
+console.log(obj.age); // undefined
+
+console.log(typeof undefined); // undefined
