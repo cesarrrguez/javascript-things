15cdffbd1cf48dfa9ef3e6b7b1686a1d902300ce
César Rodríguez <cesar.rrguez@gmail.com>
2023-11-07 22:45:59 +0100

    Add data estructures (linked list and double linked list)

---
+++ src/data-structures/double-linked-list.js
@@ -0,0 +1,48 @@
+class Node {
+  constructor(value, prev = null, next = null) {
+    this.value = value;
+    this.prev = prev;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    if(!this.head) {
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+    }
+
+    this.tail = newNode;
+  }
+
+  traverse(callback){
+    let currentNode = this.head;
+
+    while(currentNode != null) {
+      callback(currentNode);
+      currentNode = currentNode.next;
+    }
+  }
+}
+
+const printNode = (node) => console.log(node.value);
+
+const list = new LinkedList();
+list.append(1);
+list.append(2);
+list.append(3);
+
+list.traverse(printNode);
+// 1
+// 2
+// 3
---
+++ src/data-structures/linked-list.js
@@ -0,0 +1,46 @@
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    if(!this.head) {
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+    }
+
+    this.tail = newNode;
+  }
+
+  traverse(callback){
+    let currentNode = this.head;
+
+    while(currentNode != null) {
+      callback(currentNode);
+      currentNode = currentNode.next;
+    }
+  }
+}
+
+const printNode = (node) => console.log(node.value);
+
+const list = new LinkedList();
+list.append(1);
+list.append(2);
+list.append(3);
+
+list.traverse(printNode);
+// 1
+// 2
+// 3
